4b0b88bb13d18b973b94599e67a7b4cf17820b24
Agnė Klimaitė <agnaite@me.com>
2016-07-11 20:27:28 -0700

    added add_classification()

--- melons.py
+++ melons.py
@@ -1,12 +1,16 @@
+def add_classification(classification):
+    for melon in melons:
+        melons[melon][classification] = melons[melon].get(classification, None)
+
+
 melons = {
     "Honeydew": {"price": 0.99, "seedlessness": True},
     "Crenshaw": {"price": 2.00, "seedlessness": False},
     "Crane": {"price": 2.50, "seedlessness": False},
     "Casaba": {"price": 2.00, "seedlessness": False},
+    "Cantaloupe": {"price": 0.99, "seedlessness": False}
+    }
 
-    "Cantaloupe": {"price": 0.99,
-                   "seedlessness": False,
-                   "flesh_color": None,
-                   "weight": None,
-                   "rind_color": None}
-}
+add_classification("flesh_color")
+add_classification("weight")
+add_classification("rind_color")
